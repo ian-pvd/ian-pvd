09acfe13dea56b60b184ae8daa312ab8b5550a79
Ian Del Giudice <ian.pvd@gmail.com>
2018-07-24 12:52:40 -0400

    WIP front page content area

---
+++ template-parts/content-page-front.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Template part for displaying front page content
+ *
+ * @package ianpvd
+ */
+
+$post_header_class = '';
+if ( pvd_get_the_post_header() ) {
+	$post_header_class = 'post-header--has-intro';
+}
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="post-header <?php echo esc_attr( $post_header_class ) ?>">
+
+		<?php
+			if ( has_post_thumbnail() ) {
+				get_template_part( 'template-parts/featured-image' );
+			}
+		?>
+
+		<div class="post-header__content wrapper wrapper--content">
+			<?php the_title( '<h1 class="post-title">', '</h1>' ); ?>
+			<?php if ( pvd_get_the_post_header() ) : ?>
+			<div class="post-header__intro">
+				<?php pvd_the_post_header(); ?>
+			</div>
+			<?php endif; ?>
+		</div>
+
+	</header><!-- .post-header -->
+
+	<?php if ( get_the_content() ) : ?>
+	<div class="post-content wp-content wrapper wrapper--content">
+		<?php the_content(); ?>
+	</div><!-- .post-content -->
+	<?php endif; ?>
+
+</article><!-- #post-<?php the_ID(); ?> -->
