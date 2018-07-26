b6c43efdff5c384bfd4f39ec215d0a869ea2d39b
Ian Del Giudice <ian.pvd@gmail.com>
2018-07-26 12:10:57 -0400

    webpack build

--- inc/versioned-assets.php
+++ inc/versioned-assets.php
@@ -8,19 +8,19 @@
 */
 function ianpvd_get_versioned_asset( $asset ) {
 	$assets = array(
-		'site-js' => 'js/site.b464747.bundle.min.js',
-		'site-css' => 'css/site.29734669b2ddde24b0eb.min.css',
-		'page-js' => 'js/page.b464747.bundle.min.js',
-		'page-css' => 'css/page.29734669b2ddde24b0eb.min.css',
-		'home-js' => 'js/home.b464747.bundle.min.js',
-		'home-css' => 'css/home.29734669b2ddde24b0eb.min.css',
-		'article-js' => 'js/article.b464747.bundle.min.js',
-		'article-css' => 'css/article.29734669b2ddde24b0eb.min.css',
-		'archive-js' => 'js/archive.b464747.bundle.min.js',
-		'archive-css' => 'css/archive.29734669b2ddde24b0eb.min.css',
-		'admin-js' => 'js/admin.b464747.bundle.min.js',
-		'admin-css' => 'css/admin.29734669b2ddde24b0eb.min.css',
-		'common-js' => 'js/common.b464747.bundle.min.js',
+		'site-js' => 'js/site.bb5c42a.bundle.min.js',
+		'site-css' => 'css/site.d7576c70498a3bff23ec.min.css',
+		'page-js' => 'js/page.bb5c42a.bundle.min.js',
+		'page-css' => 'css/page.d7576c70498a3bff23ec.min.css',
+		'home-js' => 'js/home.bb5c42a.bundle.min.js',
+		'home-css' => 'css/home.d7576c70498a3bff23ec.min.css',
+		'article-js' => 'js/article.bb5c42a.bundle.min.js',
+		'article-css' => 'css/article.d7576c70498a3bff23ec.min.css',
+		'archive-js' => 'js/archive.bb5c42a.bundle.min.js',
+		'archive-css' => 'css/archive.d7576c70498a3bff23ec.min.css',
+		'admin-js' => 'js/admin.bb5c42a.bundle.min.js',
+		'admin-css' => 'css/admin.d7576c70498a3bff23ec.min.css',
+		'common-js' => 'js/common.bb5c42a.bundle.min.js',
 	);
 	return ! empty( $assets[ $asset ] ) ? $assets[ $asset ] : false;
 }
