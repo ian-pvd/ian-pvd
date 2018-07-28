81c837a4df0a06d6301c95d0acc79052a5477238
Ian Del Giudice <ian.pvd@gmail.com>
2018-07-28 06:08:40 -0400

    WIP v1 of blog page

--- archive.php
+++ archive.php
@@ -12,36 +12,42 @@ get_header(); ?>
 	<div id="primary" class="content-area post-archive">
 		<main id="main" class="site-main" role="main">
 
-		<?php
-		if ( have_posts() ) : ?>
+		<div class="post-list post-list--archive post-list--recent-posts post-list__wrapper">
 
-			<header class="page-header">
-				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
+		<header class="page-header">
+			<?php
+				the_archive_title( '<h1 class="post-list__header post-list__header--recent-posts">', '</h1>' );
+				// the_archive_description( '<div class="archive-description">', '</div>' );
+			?>
+		</header><!-- .page-header -->
+
+		<div class="post-list__feed post-list__feed--recent-posts post-list__feed--grid-thirds">
 
 			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
+			if ( have_posts() ) : ?>
 
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
+				<?php
+				/* Start the Loop */
+				while ( have_posts() ) : the_post();
 
-			endwhile;
+					/*
+					 * Include the Post-Format-specific template for the content.
+					 * If you want to override this in a child theme, then include a file
+					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+					 */
+					get_template_part( 'template-parts/content', 'list-tout' );
 
-			the_posts_navigation();
+				endwhile;
 
-		else :
+				the_posts_navigation();
 
-			get_template_part( 'template-parts/content', 'none' );
+			else :
 
-		endif; ?>
+				get_template_part( 'template-parts/content', 'none' );
+
+			endif; ?>
+
+		</div>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
--- index.php
+++ index.php
@@ -17,36 +17,39 @@ get_header(); ?>
 	<div id="primary" class="content-area post-archive">
 		<main id="main" class="site-main" role="main">
 
-		<?php
-		if ( have_posts() ) :
+			<div class="post-list post-list--archive post-list--recent-posts post-list__wrapper">
 
-			if ( is_home() && ! is_front_page() ) : ?>
+				<?php if ( is_home() && ! is_front_page() ) : ?>
 				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+					<h1 class="post-list__header post-list__header--recent-posts">
+						<?php single_post_title(); ?>
+					</h1>
 				</header>
+				<?php endif; ?>
 
-			<?php
-			endif;
+				<div class="post-list__feed post-list__feed--recent-posts post-list__feed--grid-thirds">
 
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
+				<?php
+					if ( have_posts() ) {
 
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
+						/* Start the Loop */
+						while ( have_posts() ) : the_post();
 
-			endwhile;
+							get_template_part( 'template-parts/content', 'list-tout' );
 
-			the_posts_navigation();
+						endwhile;
 
-		else :
+						the_posts_navigation();
 
-			get_template_part( 'template-parts/content', 'none' );
+					} else {
 
-		endif; ?>
+						get_template_part( 'template-parts/content', 'none' );
+
+					}
+				?>
+				</div>
+
+			</div>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
