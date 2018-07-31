556037e787c966956cb9c0caad87ac46d34aa4f9
Ian Del Giudice <ian.pvd@gmail.com>
2018-07-31 18:01:11 -0400

    Work archive pages

--- inc/portfolio-work/tags.php
+++ inc/portfolio-work/tags.php
@@ -44,18 +44,27 @@ function pvd_work_client_list( $work = false ) {
 /**
  * Prints the project platform.
  */
-function pvd_work_platform( $work = false ) {
+function pvd_work_platform( $work = false, $args = [] ) {
 	// If no work data provided, get post meta.
 	if ( ! $work ) {
 		$work = get_post_meta( get_the_ID(), 'work_item', true );
 	}
 
+	$defaults = [ 'links' => false, 'empty_text' => 'N/A' ];
+	$args = wp_parse_args( $args, $defaults );
+
 	// Output what we've got.
 	if ( ! empty( $work['details']['platform'] ) ) {
 		$category = get_term( $work['details']['platform'] );
-		echo '<a href="' . esc_url( get_term_link($category->term_id ) ) . '">' . $category->name . '</a>';
+		if ( true == $args['links'] ) {
+			echo '<a href="' . esc_url( get_term_link($category->term_id ) ) . '">' . $category->name . '</a>';
+		} else {
+			echo $category->name;
+		}
 	} else {
-		echo esc_html( 'N/A', 'ianpvd' );
+		if ( ! is_null( $args['empty_text'] ) ) {
+			echo esc_html( 'N/A', 'ianpvd' );
+		}
 	}
 };
 
@@ -79,7 +88,11 @@ function pvd_work_project_date( $work = false ) {
 /**
  * Outputs an html list of clients for a Work post
  */
-function pvd_work_technology_list() {
+function pvd_work_technology_list( $args = [] ) {
+	// Merge Defaults
+	$defaults = [ 'links' => false, 'empty_text' => 'N/A' ];
+	$args = wp_parse_args( $args, $defaults );
+
 	// Get Post Tags
 	$post_tags = wp_get_post_tags( get_the_ID()) ;
 
@@ -92,23 +105,47 @@ function pvd_work_technology_list() {
 	}
 
 	// Start list markup
-	echo '<ul class="post-meta__list">';
+	$list = '<ul class="post-meta__list">';
 
 	// Loop through client array to output list.
 	foreach ($tech_array as $item => $technology ) {
 
 		// Check if terms have WP_Object IDs set
 		if ( isset( $technology->term_id ) ) {
-			$technology = '<a href="' . esc_url( get_term_link($technology->term_id ) ) . '">' . $technology->name . '</a>';
+			if ( true == $args['links'] ) {
+				$technology = '<a href="' . esc_url( get_term_link($technology->term_id ) ) . '">' . $technology->name . '</a>';
+			} else {
+				$technology = esc_html( $technology->name );
+			}
 		} else {
-			// If no term_ID, likely the fallback term object
-			$technology = esc_html( $technology->name );
+			if ( ! is_null( $args['empty_text'] ) ) {
+				// If no term_ID, likely the fallback term object
+				$technology = esc_html( $technology->name );
+			} else {
+				return;
+			}
 		}
 
 		// Add what we've got to the list:
-		echo '<li class="post-meta__list-item">' . $technology . '</li>';
+		$list .= '<li class="post-meta__list-item">' . $technology . '</li>';
 	}
 
 	// Close list markup
-	echo '</ul>';
+	$list .= '</ul>';
+
+	echo $list;
+}
+
+/**
+ * Returns the first project link as the permalink
+ *
+ * TODO: v2, check for 'Link directly to project' and link to work-single or external
+ */
+function pvd_work_permalink( $work = false ) {
+	// If no work data provided, get post meta.
+	if ( ! $work ) {
+		$work = get_post_meta( get_the_ID(), 'work_item', true );
+	}
+
+	return $work['links']['primary_link'];
 }
