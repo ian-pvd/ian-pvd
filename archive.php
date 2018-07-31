156ea70603dfbb1bad4c02d812e55f588ac7dd82
Ian Del Giudice <ian.pvd@gmail.com>
2018-07-31 16:17:44 -0400

    archive post navigation

--- archive.php
+++ archive.php
@@ -11,13 +11,13 @@ get_header(); ?>
 
 	<div id="primary" class="content-area post-archive">
 		<main id="main" class="site-main" role="main">
-			<div class="post-list post-list--blog post-list--archive post-list__wrapper">
+			<div class="post-list post-list--archive post-list__wrapper">
 
 				<header class="archive-header">
 					<?php the_archive_title( '<h1 class="archive-title">', '</h1>' ); ?>
 				</header><!-- .page-header -->
 
-				<div class="post-list__feed post-list__feed--blog post-list__feed--grid-thirds">
+				<div class="post-list__feed post-list__feed--archive post-list__feed--grid-thirds">
 
 				<?php
 				if ( have_posts() ) : ?>
