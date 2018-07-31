f8514d1089b6064f75c081214590e2807ac1ef0c
Ian Del Giudice <ian.pvd@gmail.com>
2018-07-31 01:55:43 -0400

    CR cleanup

--- archive.php
+++ archive.php
@@ -12,42 +12,36 @@ get_header(); ?>
 	<div id="primary" class="content-area post-archive">
 		<main id="main" class="site-main" role="main">
 
-		<div class="post-list post-list--archive post-list--recent-posts post-list__wrapper">
+		<?php
+		if ( have_posts() ) : ?>
 
-		<header class="page-header">
-			<?php
-				the_archive_title( '<h1 class="post-list__header post-list__header--recent-posts">', '</h1>' );
-				// the_archive_description( '<div class="archive-description">', '</div>' );
-			?>
-		</header><!-- .page-header -->
-
-		<div class="post-list__feed post-list__feed--recent-posts post-list__feed--grid-thirds">
-
-			<?php
-			if ( have_posts() ) : ?>
-
+			<header class="page-header">
 				<?php
-				/* Start the Loop */
-				while ( have_posts() ) : the_post();
+					the_archive_title( '<h1 class="page-title">', '</h1>' );
+					the_archive_description( '<div class="archive-description">', '</div>' );
+				?>
+			</header><!-- .page-header -->
 
-					/*
-					 * Include the Post-Format-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-					get_template_part( 'template-parts/content', 'list-tout' );
+			<?php
+			/* Start the Loop */
+			while ( have_posts() ) : the_post();
 
-				endwhile;
+				/*
+				 * Include the Post-Format-specific template for the content.
+				 * If you want to override this in a child theme, then include a file
+				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+				 */
+				get_template_part( 'template-parts/content', get_post_format() );
 
-				the_posts_navigation();
+			endwhile;
 
-			else :
+			the_posts_navigation();
 
-				get_template_part( 'template-parts/content', 'none' );
+		else :
 
-			endif; ?>
+			get_template_part( 'template-parts/content', 'none' );
 
-		</div>
+		endif; ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
