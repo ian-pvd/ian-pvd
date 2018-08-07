88880e2a53b771cd6100b5255f1a3f9b6cac5332
Ian Del Giudice <ian.pvd@gmail.com>
2018-08-07 02:57:40 -0400

    QA home page content

--- inc/media.php
+++ inc/media.php
@@ -13,3 +13,11 @@ function pvd_image_sizes() {
 	add_image_size( 'portfolio', 512, 288, [ 'center', 'top' ] );
 }
 add_action( 'after_setup_theme', 'pvd_image_sizes' );
+
+/**
+ * Unwrap paragraph tags on images in WP Content
+ */
+function pvd_unwrap_img_content($content) {
+   return preg_replace('/<p>\s*(<a .*>)?\s*(<img .* \/>)\s*(<\/a>)?\s*<\/p>/iU', '\1\2\3', $content);
+}
+add_filter('the_content', 'pvd_unwrap_img_content');
