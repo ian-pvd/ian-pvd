a5ec55dce13a5af0d59d3aa662dc4c9c464fe499
Ian Del Giudice <ian.pvd@gmail.com>
2018-08-31 15:54:30 -0400

    webpack build

--- inc/versioned-assets.php
+++ inc/versioned-assets.php
@@ -8,21 +8,21 @@
 */
 function ianpvd_get_versioned_asset( $asset ) {
 	$assets = array(
-		'work-js' => 'js/work.97b402a.bundle.min.js',
-		'work-css' => 'css/work.24a97ad27d0ea18b2ce8.min.css',
-		'site-js' => 'js/site.97b402a.bundle.min.js',
-		'site-css' => 'css/site.24a97ad27d0ea18b2ce8.min.css',
-		'page-js' => 'js/page.97b402a.bundle.min.js',
-		'page-css' => 'css/page.24a97ad27d0ea18b2ce8.min.css',
-		'home-js' => 'js/home.97b402a.bundle.min.js',
-		'home-css' => 'css/home.24a97ad27d0ea18b2ce8.min.css',
-		'article-js' => 'js/article.97b402a.bundle.min.js',
-		'article-css' => 'css/article.24a97ad27d0ea18b2ce8.min.css',
-		'archive-js' => 'js/archive.97b402a.bundle.min.js',
-		'archive-css' => 'css/archive.24a97ad27d0ea18b2ce8.min.css',
-		'admin-js' => 'js/admin.97b402a.bundle.min.js',
-		'admin-css' => 'css/admin.24a97ad27d0ea18b2ce8.min.css',
-		'common-js' => 'js/common.97b402a.bundle.min.js',
+		'work-js' => 'js/work.06ea3d1.bundle.min.js',
+		'work-css' => 'css/work.7e5cc3175b06f3b4800b.min.css',
+		'site-js' => 'js/site.06ea3d1.bundle.min.js',
+		'site-css' => 'css/site.7e5cc3175b06f3b4800b.min.css',
+		'page-js' => 'js/page.06ea3d1.bundle.min.js',
+		'page-css' => 'css/page.7e5cc3175b06f3b4800b.min.css',
+		'home-js' => 'js/home.06ea3d1.bundle.min.js',
+		'home-css' => 'css/home.7e5cc3175b06f3b4800b.min.css',
+		'article-js' => 'js/article.06ea3d1.bundle.min.js',
+		'article-css' => 'css/article.7e5cc3175b06f3b4800b.min.css',
+		'archive-js' => 'js/archive.06ea3d1.bundle.min.js',
+		'archive-css' => 'css/archive.7e5cc3175b06f3b4800b.min.css',
+		'admin-js' => 'js/admin.06ea3d1.bundle.min.js',
+		'admin-css' => 'css/admin.7e5cc3175b06f3b4800b.min.css',
+		'common-js' => 'js/common.06ea3d1.bundle.min.js',
 	);
 	return ! empty( $assets[ $asset ] ) ? $assets[ $asset ] : false;
 }
