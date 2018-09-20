318648c24d4db800fa57b6337a237a301d654c42
Ian Del Giudice <ian.pvd@gmail.com>
2018-09-20 03:14:20 -0400

    Recommended: Use Babel config for Prism

--- client/js/article/article.js
+++ client/js/article/article.js
@@ -3,14 +3,6 @@
 // Import PrismJS package
 import Prism from 'prismjs';
 
-// Import PrismJS extensions
-import 'prismjs/themes/prism-twilight.css';
-import 'prismjs/components/prism-scss';
-
-// Import Prism JS
-import 'prismjs/plugins/line-numbers/prism-line-numbers';
-import 'prismjs/plugins/line-numbers/prism-line-numbers.css';
-
 // Article Styles
 import '../../scss/article/index.scss';
 
