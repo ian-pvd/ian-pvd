445cab97cd8a08e4e347d463299ab9bc2b51faf1
Ian Del Giudice <ian.pvd@gmail.com>
2018-09-20 03:02:30 -0400

    Include PrismJS in article entry point

--- client/js/article/article.js
+++ client/js/article/article.js
@@ -1,4 +1,10 @@
 /* Article JS */
 
+// Import PrismJS package
+import Prism from 'prismjs';
+
 // Article Styles
 import '../../scss/article/index.scss';
+
+// Highlight all matching syntax
+Prism.highlightAll();
