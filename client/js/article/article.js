9c6762ad72d3228b1038e89ae840aa82f6355bb2
Ian Del Giudice <ian.pvd@gmail.com>
2018-09-20 03:31:53 -0400

    Sync additional PrismJS customizations

--- client/js/article/article.js
+++ client/js/article/article.js
@@ -1,12 +1,9 @@
 /* Article JS */
 
-// Import prismjs package
+// Import PrismJS package
 import Prism from 'prismjs';
 
 // Article Styles
 import '../../scss/article/index.scss';
 
 Prism.highlightAll();
-
-// Add language name to pre elements with a language class set.
-// const codeBlocks = document.querySelectorAll('pre[class*="language-"]');
